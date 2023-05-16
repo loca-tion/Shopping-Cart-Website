f8926f433271ac6ebecbd9d5c3673fb50db0d91f
Sujal Sharma <95977443+loca-tion@users.noreply.github.com>
2023-05-16 11:13:52 +0530

    Add files via upload

---
+++ src/Product.js
@@ -0,0 +1,26 @@
+export const PRODUCTS = [
+    {
+        id:1,
+        productName:"Camera",
+        price:999.0,
+        productImage: 'https://images.unsplash.com/photo-1502920917128-1aa500764cbd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+    },
+    {
+        id:2,
+        productName:"Iphone",
+        price:1999.0,
+        productImage: 'https://images.unsplash.com/photo-1591337676887-a217a6970a8a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80',
+    },
+    {
+        id:3,
+        productName:"MacBook",
+        price:999.0,
+        productImage: 'https://images.unsplash.com/photo-1616400619175-5beda3a17896?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80',
+    },
+    {
+        id:4,
+        productName:"Zara",
+        price:999.0,
+        productImage: 'https://images.unsplash.com/photo-1605648511528-fa48bdb09d95?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+    }
+]
